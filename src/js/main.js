cfb096e77d7d7b0fae2bc97baa50cf6f59846d3c
Andrey <Sibir_91@list.ru>
2020-07-15 15:38:28 +0400

    Fixed Media

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@
 gsap.from(".logo", {opacity: 0, x: -500, ease: "power2.out", duration: 1, delay: 0.4});
 gsap.from(".promo__title", {opacity: 0, x: -500, ease: "power2.out", duration: 1, delay: 0.6});
 gsap.from(".promo__sub-title", {opacity: 0, x: -500, ease: "power2.out", duration: 1, delay: 0.8});
-gsap.from(".promo__btn", {opacity: 0, scale: 2, ease: "power2.out", duration: 1, delay: 1.6});
+gsap.from(".btn", {opacity: 0, scale: 2, ease: "power2.out", duration: 1, delay: 1.6});
 gsap.from(".country__item", {opacity: 0, y: -500, ease: "power2.out", duration: 1, delay: 2, stagger: 0.2,});
 gsap.to('.contact__icon', {scale: 1.1, duration: 0.6, yoyo: true, repeat: -1});
 gsap.to('.with-prengi__arrow-down', {y:-10, yoyo: true, repeat: -1});
