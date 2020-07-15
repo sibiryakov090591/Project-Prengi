26d2d6556fef426294d9eeb5f039e3d5a4c820a4
Andrey <Sibir_91@list.ru>
2020-07-15 14:23:59 +0400

    Add Modal

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,8 @@
 gsap.from(".logo", {opacity: 0, x: -500, ease: "power2.out", duration: 1, delay: 0.4});
 gsap.from(".promo__title", {opacity: 0, x: -500, ease: "power2.out", duration: 1, delay: 0.6});
 gsap.from(".promo__sub-title", {opacity: 0, x: -500, ease: "power2.out", duration: 1, delay: 0.8});
-gsap.from(".country__item", {opacity: 0, y: -500, ease: "power2.out", duration: 1, delay: 1, stagger: 0.2,});
-gsap.from(".promo__btn", {opacity: 0, scale: 2, ease: "power2.out", duration: 1, delay: 2.8});
+gsap.from(".promo__btn", {opacity: 0, scale: 2, ease: "power2.out", duration: 1, delay: 1.6});
+gsap.from(".country__item", {opacity: 0, y: -500, ease: "power2.out", duration: 1, delay: 2, stagger: 0.2,});
 gsap.to('.contact__icon', {scale: 1.1, duration: 0.6, yoyo: true, repeat: -1});
 gsap.to('.with-prengi__arrow-down', {y:-10, yoyo: true, repeat: -1});
 
@@ -38,6 +38,14 @@ $(document).ready(function () {
 
     $('.mobile-menu').click(function(event) {
         $('.mobile-menu, .menu').toggleClass('active');
+    });
+
+    $('[data-modal=consultation]').click(function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').click(function() {
+        $('.overlay, #consultation').fadeOut('slow');
     });
 
 });
